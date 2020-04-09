9164a547636523190150913347601ebae982c46e
unknown <rmtestard@gmail.com>
2020-04-09 20:26:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import userCustomStyles from './usage.css';
+import {
+  MediumClap,
+  ClapIcon,
+  ClapCount,
+  CountTotal,
+} from './components/ControlledProps';
+
+const INITIAL_STATE = {
+  count: 0,
+  countTotal: 2100,
+  isClicked: false,
+};
+
+const MAXIMUM_CLAP_VALUE = 10;
 
 function App() {
+  const [state, setState] = useState(INITIAL_STATE);
+  const handleClap = () => {
+    console.log(state)
+    setState(({ count, countTotal }) => ({
+      count: Math.min(count + 1, MAXIMUM_CLAP_VALUE),
+      countTotal: count < MAXIMUM_CLAP_VALUE ? countTotal + 1 : countTotal,
+      isClick: true,
+    }));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100%' }}>
+      <MediumClap
+        values={state}
+        onClap={handleClap}
+        style={{ border: '1px solid orange' }}
+      >
+        <ClapIcon className={userCustomStyles.icon} />
+        <ClapCount className={userCustomStyles.count} />
+        <CountTotal className={userCustomStyles.total} />
+      </MediumClap>
+      <MediumClap
+        values={state}
+        onClap={handleClap}
+        style={{ border: '1px solid orange' }}
+      >
+        <ClapIcon className={userCustomStyles.icon} />
+        <ClapCount className={userCustomStyles.count} />
+        <CountTotal className={userCustomStyles.total} />
+      </MediumClap>
     </div>
   );
 }
